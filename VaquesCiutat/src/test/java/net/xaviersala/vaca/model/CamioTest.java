303d2958c7ec38ecf557a9a52a7e8bf616b71ed9
Maribel Madueño <mmaduen4@xtec.cat>
2020-07-02 16:35:23 +0200

    Creació de CamioTest.

---
+++ VaquesCiutat/src/test/java/net/xaviersala/vaca/model/CamioTest.java
@@ -0,0 +1,14 @@
+package net.xaviersala.vaca.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CamioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
