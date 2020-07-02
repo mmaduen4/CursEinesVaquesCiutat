c0863071f868f9f020422465e9ef6556f7f0f250
Maribel Madueño <mmaduen4@xtec.cat>
2020-07-02 16:36:36 +0200

    testEntraVacaAmbPesSotaOIgualALimit

--- VaquesCiutat/src/test/java/net/xaviersala/vaca/model/CamioTest.java
+++ VaquesCiutat/src/test/java/net/xaviersala/vaca/model/CamioTest.java
@@ -2,13 +2,41 @@ package net.xaviersala.vaca.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
 
 class CamioTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+	
+
+	static Stream<Arguments> createPesMaxCamioPesVacaOK() {
+		return Stream.of(Arguments.of(1000, 100), Arguments.of(1000, 900), Arguments.of(1000, 1000));
+	}
+
+	@ParameterizedTest
+	@MethodSource("createPesMaxCamioPesVacaOK")
+	void testEntraVacaAmbPesSotaOIgualALimit(double pesMaxim, double pesVaca) {
+		// ARRANGE
+		Vaca vacaFalsa = Mockito.mock(Vaca.class);
+		Mockito.when(vacaFalsa.getPes()).thenReturn(pesVaca);
+
+		Camio sut = new Camio(pesMaxim);
+		double pesActual = sut.getPesActual();
+
+		// ACT
+		boolean result = sut.entraVaca(vacaFalsa);
+
+		// ASSERT
+		assertTrue(result);
+		assertEquals(pesActual + pesVaca, sut.getPesActual());
+
 	}
 
 }
