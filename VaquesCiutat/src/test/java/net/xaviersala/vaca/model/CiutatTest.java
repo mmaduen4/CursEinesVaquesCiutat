22de779ca4a7ce5b63d65153d2d92749ab6d2e3b
Maribel Madueño <mmaduen4@xtec.cat>
2020-07-03 14:12:45 +0200

    Modificació tests per incloure en el preu si el nombre de litres és senar/parell.

--- VaquesCiutat/src/test/java/net/xaviersala/vaca/model/CiutatTest.java
+++ VaquesCiutat/src/test/java/net/xaviersala/vaca/model/CiutatTest.java
@@ -16,30 +16,54 @@ class CiutatTest {
 //		fail("Not yet implemented");
 //	}
 		
-	static Stream<Arguments> createLitresCamio() {
-		return Stream.of(Arguments.of(0),Arguments.of(20.0),Arguments.of(102.0),Arguments.of(204.0),Arguments.of(305.0),Arguments.of(400.0));
+	static Stream<Arguments> createLitresCamioParell() {
+		return Stream.of(Arguments.of(0.0),Arguments.of(20.0),Arguments.of(102.0),Arguments.of(204.0),Arguments.of(306.0),Arguments.of(400.0));
 	}
-
+	
 	@ParameterizedTest
-	@MethodSource("createLitresCamio")
-	void testPreuCamioSenseLletCiutat(double litresCamio) {
+	@MethodSource("createLitresCamioParell")
+	void testPreuCamioLitresParellSenseLletCiutat(double litresCamio) {
 		
 		double preuBaseLlet=1.1;
 		
 		// ARRANGE		
 		Camio camioFals1 = Mockito.mock(Camio.class);
 		Mockito.when(camioFals1.getLitres()).thenReturn(litresCamio);
-		
+		Mockito.when(camioFals1.litresEsParell()).thenReturn(1);
 		Ciutat ciutat =new Ciutat();
 		
 		// ACT
 		double resultat = ciutat.getPreuPagarCamio(camioFals1);
-
-		// ASSERT
-		assertEquals(litresCamio*preuBaseLlet,resultat);
+		
+		// ASSERT		
+		assertEquals(litresCamio*(preuBaseLlet+0.1),resultat,0.01F);
 
 	}	
 	
+	static Stream<Arguments> createLitresCamioSenar() {
+		return Stream.of(Arguments.of(1.0),Arguments.of(21.0),Arguments.of(103.0),Arguments.of(205.0),Arguments.of(305.0),Arguments.of(401.0));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("createLitresCamioSenar")
+	void testPreuCamioLitresSenarSenseLletCiutat(double litresCamio) {
+		
+		double preuBaseLlet=1.1;
+		
+		// ARRANGE		
+		Camio camioFals1 = Mockito.mock(Camio.class);
+		Mockito.when(camioFals1.getLitres()).thenReturn(litresCamio);
+		Mockito.when(camioFals1.litresEsParell()).thenReturn(-1);
+		Ciutat ciutat =new Ciutat();
+		
+		// ACT
+		double resultat = ciutat.getPreuPagarCamio(camioFals1);
+		
+		// ASSERT		
+		assertEquals(litresCamio*(preuBaseLlet-0.1),resultat,0.01F);
+
+	}	
+		
 	static Stream<Arguments> createLitres2Camions() {
 	return Stream.of(Arguments.of(0.00,100.00),Arguments.of(0.00,101.00),Arguments.of(0.00,202.00),
 					 Arguments.of(102.0,100.00),Arguments.of(102.0,101.00),Arguments.of(102.0,202.00),
@@ -48,7 +72,7 @@ class CiutatTest {
 	
 	@ParameterizedTest
 	@MethodSource("createLitres2Camions")
-	void testPreuCamioAmbLletCiutat(double litresCamio1,double litresCamio2) {
+	void testPreuCamioLitresSenarAmbLletCiutat(double litresCamio1,double litresCamio2) {
 		
 		double preuBaseLlet=1.1;
 		
@@ -58,6 +82,7 @@ class CiutatTest {
 		
 		Camio camioFals2 = Mockito.mock(Camio.class);
 		Mockito.when(camioFals2.getLitres()).thenReturn(litresCamio2);
+		Mockito.when(camioFals1.litresEsParell()).thenReturn(-1);
 		
 		Ciutat ciutat =new Ciutat();
 		
@@ -66,9 +91,36 @@ class CiutatTest {
 		double resultat = ciutat.getPreuPagarCamio(camioFals2);
 		
 		// ASSERT		
-		assertEquals(litresCamio2*(preuBaseLlet-(int)(litresCamio1/100)*0.1),resultat,0.01F);
+		assertEquals(litresCamio2*(preuBaseLlet-0.1-(int)(litresCamio1/100)*0.1),resultat,0.01F);
 
 	}	
+	
+	@ParameterizedTest
+	@MethodSource("createLitres2Camions")
+	void testPreuCamioLitresParellAmbLletCiutat(double litresCamio1,double litresCamio2) {
+		
+		double preuBaseLlet=1.1;
+		
+		// ARRANGE		
+		Camio camioFals1 = Mockito.mock(Camio.class);
+		Mockito.when(camioFals1.getLitres()).thenReturn(litresCamio1);
+		
+		Camio camioFals2 = Mockito.mock(Camio.class);
+		Mockito.when(camioFals2.getLitres()).thenReturn(litresCamio2);
+		Mockito.when(camioFals1.litresEsParell()).thenReturn(1);
+		
+		Ciutat ciutat =new Ciutat();
+		
+		// ACT
+		ciutat.getPreuPagarCamio(camioFals1);
+		double resultat = ciutat.getPreuPagarCamio(camioFals2);
+		
+		// ASSERT		
+		assertEquals(litresCamio2*(preuBaseLlet+0.1-(int)(litresCamio1/100)*0.1),resultat,0.01F);
+
+	}
+	
+	
 	
 	static Stream<Arguments> createLitres3Camions() {
 	return Stream.of(Arguments.of(900.0,100.00,100.00),Arguments.of(900.0,101.00,100.00),Arguments.of(900.0,202.00,100.00));
